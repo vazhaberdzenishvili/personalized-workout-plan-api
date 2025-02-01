518204428648b7c0636bbbaf25a0691547205f5f
berdzenav <berdzenav@gmail.com>
2025-02-01 13:13:32 +0400

    feat: Implement JWT authentication for user login

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,10 +4,23 @@ URL mappings for the user API.
 from django.urls import path
 
 from user import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -4,8 +4,22 @@ Views for the user API.
 from rest_framework import generics
 
 from user.serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
