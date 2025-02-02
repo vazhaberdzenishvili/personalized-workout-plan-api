34c93c7433f85f11151a7db6c590d0f7de067ef9
berdzenav <berdzenav@gmail.com>
2025-02-02 16:08:54 +0400

    chore: Add JWT authentication to REST framework settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -138,6 +138,9 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 SIMPLE_JWT = {
