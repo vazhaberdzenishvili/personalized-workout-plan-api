a306eb67508289c924d0530ebc31b37d6a3df362
berdzenav <berdzenav@gmail.com>
2025-02-02 12:11:57 +0400

    feat: Add Exercise and MuscleGroup models

--- app/core/models.py
+++ app/core/models.py
@@ -40,3 +40,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class MuscleGroup(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    instructions = models.TextField()
+    target_muscles = models.ManyToManyField(MuscleGroup)
+
+    def __str__(self):
+        return self.name
